f4c315e6d186010cf255608e84c39d1277e6055e
Rebecca-alu <r.benegusen@alustudent.com>
2026-06-16 14:34:32 +0200

    Create project directory structure

--- setup_project.sh
+++ setup_project.sh
@@ -1,5 +1,13 @@
+
 #!/bin/bash
 
 read -p "Enter project name: " name
 
 project_dir="attendance_tracker_$name"
+
+mkdir -p "$project_dir/Helpers"
+
+mkdir -p "$project_dir/reports"
+
+echo "Folders created"
+
