d49578618790b642693ec381c9bf1c69c6d995fd
Rebecca-alu <r.benegusen@alustudent.com>
2026-06-16 14:35:26 +0200

    Generate configuration file

--- setup_project.sh
+++ setup_project.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 read -p "Enter project name: " name
@@ -6,8 +5,17 @@ read -p "Enter project name: " name
 project_dir="attendance_tracker_$name"
 
 mkdir -p "$project_dir/Helpers"
-
 mkdir -p "$project_dir/reports"
 
-echo "Folders created"
+cat > "$project_dir/Helpers/config.json" << EOF2
+{
+    "thresholds": {
+        "warning": 75,
+        "failure": 50
+    },
+    "run_mode": "live",
+    "total_sessions": 15
+}
+EOF2
 
+echo "Config file created"
